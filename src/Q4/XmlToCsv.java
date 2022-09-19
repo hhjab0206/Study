c74b751e122eba7adabf62368b47ac4baf01c108
nets_hsj <hhjab0206@naver.com>
2022-09-19 17:28:04 +0900

    피드백 후 수정

--- src/Q4/XmlToCsv.java
+++ src/Q4/XmlToCsv.java
@@ -6,8 +6,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,54 +14,45 @@ public class XmlToCsv
 {
 	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException
 	{
+		NodeList nodeList = getNodeList(new File("C:/study/Study/src/Q4/music.xml"));
+		writeCsv(nodeList, new File("C:/study/Study/src/Q4/output.csv"));
+	}
+	private static void writeCsv(NodeList list, File outputFile) throws IOException
+	{
+		List<String> headers = getHeaders(list);
+
+		try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(outputFile))))
+		{
+			pw.println(String.join(",", headers));
+
+			List<String> cols = new ArrayList<>();
+			for (int i = 0; i < list.getLength(); i++)
+			{
+				Element element = (Element) list.item(i);
+				for (String header : headers)
+					cols.add(element.getAttribute(header));
+
+				pw.println(String.join(",",cols));
+				cols.clear();
+			}
+		}
+	}
+	private static List<String> getHeaders(NodeList list)
+	{
+		List<String> headers = new ArrayList<>();
+		NamedNodeMap attrs = list.item(0).getAttributes();
+		for (int i = 0; i < attrs.getLength(); i++)
+			headers.add(attrs.item(i).getNodeName());
+		return headers;
+	}
+	private static NodeList getNodeList(File input) throws ParserConfigurationException, SAXException, IOException
+	{
 		// xml 문서 파싱 준비
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document document = builder.parse("C:/study/Study/src/Q4/music.xml");
+		Document document = builder.parse(input);
 
-
-		// root 구하기 (melon-top-10)
-		System.out.println(document.getDocumentElement().getNodeName());
-		FileWriter writer = new FileWriter("C:/study/Study/src/Q4/output.csv");
-		writer.write(document.getDocumentElement().getNodeName()+"\n");
-
-		// 첫번째 자식은 무조건 #text
-		Node firstChild = document.getDocumentElement().getFirstChild();
-		// 자식 노드 이름 가져오기 (music)
-		String sibling = firstChild.getNextSibling().getNodeName();
-		System.out.println(sibling);
-		writer.write(sibling+"\n");
-
-		// elementList = music 10개
-		NodeList elementList = document.getElementsByTagName(sibling);
-
-		// attributes = album, artist, genre, release-date, title
-		NamedNodeMap attributes = elementList.item(0).getAttributes();
-
-		List<String> header = new ArrayList<>();
-		for (int i=0; i<attributes.getLength(); i++)
-		{
-			Node attr = attributes.item(i);
-			header.add(attr.getNodeName());
-		}
-		System.out.println(String.join(",",header));
-		writer.write(String.join(",",header)+"\n");
-
-		List<String> values = new ArrayList<>();
-		for (int i=0; i< elementList.getLength(); i++)
-		{
-			// Node node = musicList.item(i);
-			Element element = (Element) elementList.item(i);
-			for (int j = 0; j<attributes.getLength(); j++)
-			{
-				Node attr = attributes.item(j);
-				values.add(element.getAttribute(attr.getNodeName()));
-			}
-			System.out.println(String.join(",", values));
-			writer.write(String.join(",", values)+"\n");
-			values.clear();
-		}
-		writer.flush();
-		writer.close();
+		Element root = document.getDocumentElement();
+		return root.getElementsByTagName("music");
 	}
 }
